51ea4c6a0ca9e2c1874da42aa8da4cde047562bd
Connor Hayward <connor.hayward@hotmail.co.uk>
2019-02-18 14:51:25 +0100

    Change value type for set_auto_start_state to Boolean

--- src/Devices/iSeqControl.jl
+++ src/Devices/iSeqControl.jl
@@ -469,8 +469,9 @@ end
 
 export show_auto_start_state
 
-function set_auto_start_state(device::NHQ_Module,channel::Symbol,value::Int)
+function set_auto_start_state(device::NHQ_Module,channel::Symbol,value::Boolean)
     chn = get_channel(channel)
+    value = 8*Int(value)
     cmd = "A1=$((lpad(value, 1, "0")))"
     set(device, cmd)
 end
